b0e77fa2d6e185cedf486109ccd5ef39cfeedb90
Seth Barberee <seth.barberee@gmail.com>
2024-09-07 11:04:03 -0700

    more decomp and documentation

--- src/code_8069D4C.c
+++ src/code_8069D4C.c
@@ -1,25 +1,7 @@
 #include "global.h"
 #include "pokemon_mid.h"
 #include "dungeon_util.h"
-
-struct unkStruct_8069D4C 
-{
-    s16 id;
-    Position pos;
-    s16 HP;
-    u8 atk;
-    u8 spAtk;
-    u8 def;
-    u8 spDef;
-    u16 level;
-    u32 exp;
-    u32 belly;
-    u32 maxBelly;
-    Item heldItem;
-    Move moves[MAX_MON_MOVES];
-    u32 unk40; // strugglemoveFlags?
-    HiddenPower hiddenPower;
-};
+#include "structs/struct_8069D4C.h"
 
 u32 sub_8069D18(Position *pos,Entity *entity)
 {
@@ -49,15 +31,14 @@ void sub_8069D4C(struct unkStruct_8069D4C *r0, Entity *target)
 
     r0->level = info->level;
 
-
     GetPokemonLevelData(&leveldata, info->id, info->level);
     r0->exp = leveldata.expRequired;
-    r0->atk = info->atk;
-    r0->spAtk = info->spAtk;
-    r0->def = info->def;
-    r0->spDef = info->spDef;
+    r0->att[0] = info->atk;
+    r0->att[1] = info->spAtk;
+    r0->def[0] = info->def;
+    r0->def[1] = info->spDef;
     r0->heldItem = info->heldItem;
-    memcpy(r0->moves, info->moves, sizeof(r0->moves) + sizeof(r0->unk40));
+    memcpy(r0->moves.moves, info->moves, sizeof(r0->moves));
     r0->belly = info->belly;
     r0->maxBelly = info->maxBelly;
     r0->hiddenPower = info->hiddenPower;
